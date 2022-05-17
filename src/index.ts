5a0794a093c20499c173eaf72482e40d254b780e
unknown <jacobdcutts@gmail.com>
2022-05-17 22:57:07 +0100

    scale the wheels

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import {
 
 } from 'three';
 import { createAddBoxCluster } from './boxCluster';
+import { dumpObjectToConsoleAsString } from './debugUtils';
 import { loadModel } from './loadModel';
 import { setupCamera } from './setupCamera';
 import { setupHelpers } from './setupHelpers';
@@ -24,6 +25,12 @@ export async function setupThreeJSScene() {
     const camera = setupCamera(dimensions);
 
     const renderer = setupRenderer(camera, dimensions);
+
+    const mouse = { x: 0, y: 0 };
+    renderer.domElement.addEventListener("mousemove", (event) => {
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+    })
 
     // const controls = setupOrbitControls(camera, renderer.domElement);
 
@@ -41,12 +48,20 @@ export async function setupThreeJSScene() {
     if (model) {
         model.scale.set(5, 5, 5)
         model.rotation.y += Math.PI;
+        model.traverse((child) => {
+            if (child.name.startsWith("wheel")) {
+                child.scale.multiplyScalar(10);
+            }
+
+        })
         scene.add(model);
     }
 
     let frameCount = 0;
 
     animate();
+
+    dumpObjectToConsoleAsString(model!);
 
     function animate() {
         // centreCube.rotation.y += 0.01;
@@ -56,9 +71,13 @@ export async function setupThreeJSScene() {
         // }
         if (model) {
             model.position.z -= 0.1;
-            camera.position.z = model.position.z + 30 * Math.sin(frameCount / 200);
-            camera.position.x = model.position.x + 30 * Math.cos(frameCount / 200)
+            //const t = map(mouse.x, 0, dimensions.w, 0, 2*Math.PI) //TODO get element using dom
+            const t = (mouse.x / 1920) * 2 * Math.PI;
+            camera.position.z = model.position.z + 30 * Math.sin(t);
+            camera.position.x = model.position.x + 30 * Math.cos(t);
+            camera.position.y = 50 * (1 - (mouse.y / 1080))  //50 ==> 0
             camera.lookAt(model.position);
+
         }
         const infoEl = document.getElementById('info')
         if (infoEl) {
